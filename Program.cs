9e9320ad2a4cbff03c0ef4d1159d7fe32cad9ed2
Marcelo Martins <dw.cv.mj@gmail.com>
2020-06-12 18:21:39 -0300

    Fibonacci project started with range of 0 to 350

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codenation.Challenge
+{
+    public class Math
+
+    {
+        public List<int> Fibonnaci()
+        {
+            List<int> newNumber = new List<int> { 0 };
+
+            int number = 0, controller = 1;
+
+            while (number <= 350)
+            {
+                number = number + controller;
+                if (number > 350) { break; }
+                newNumber.Add(number);
+                controller = number - controller;
+            }
+            return newNumber;
+        }
+        public bool IsFibonnaci(int inputNumber)
+        {
+            return Fibonnaci().Contains(inputNumber);
+
+        }
+    }
+}
